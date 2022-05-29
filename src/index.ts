2acda97d4acad7d89e67f3cad9197dad335b2664
Marcus Pöhls <marcus.poehls@gmail.com>
2022-05-29 14:28:10 +0200

    add implementation

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+'use strict'
+
+/**
+ * Defines the shape of a macro function.
+ */
+type MacroFn = (...args: any[]) => any
+
+export class Macroable {
+  /**
+   * Index signature.
+   */
+  [key: string]: any
+
+  /**
+   * The registered macros.
+   */
+  protected static macros: Map<string, MacroFn> = new Map()
+
+  /**
+   * Add a macro with the given `name` to this class. The related `callback`
+   * method will be assigned to the class’s prototype without you manually
+   * adding it. It’s basically a function that can be removed at any time.
+   *
+   * @example
+   * ```js
+   * Macroable.macro('getUsername', () => {
+   *   return 'supercharge'
+   * })
+   * ```
+   */
+  public static macro<T extends typeof Macroable> (name: string, callback: MacroFn): T {
+    this.validateMacro(name, callback)
+
+    this.prototype[name] = callback
+    this.macros.set(name, callback)
+
+    return this as T
+  }
+
+  /**
+   * Ensures the given `name` and `callback` are valid macro values.
+   *
+   * @param name
+   * @param callback
+   *
+   * @throws
+   */
+  private static validateMacro (name: string, callback: MacroFn): void {
+    if (!name) {
+      throw new Error('The first argument of ".macro(name, callback)" must be a valid string')
+    }
+
+    if (typeof callback !== 'function') {
+      throw new Error('The second argument ".macro(name, callback)" must be a function')
+    }
+  }
+
+  /**
+   * Determine whether a macro is registered.
+   *
+   * @param name
+   */
+  public static hasMacro (name: string): boolean {
+    return this.macros.has(name)
+  }
+
+  /**
+   * Returns the registered macro for the given `name`.
+   *
+   * @param name
+   */
+  public static getMacro (name: string): MacroFn | undefined {
+    return this.macros.get(name)
+  }
+
+  /**
+   * Determine whether a macro is registered.
+   *
+   * @param name
+   */
+  public static flushMacros<T extends typeof Macroable> (): T {
+    for (const key of this.macros.keys()) {
+      Reflect.deleteProperty(this.prototype, key)
+    }
+
+    this.macros = new Map()
+
+    return this as T
+  }
+}
---
+++ test/macroable.js
@@ -0,0 +1,87 @@
+'use strict'
+
+const { expect } = require('expect')
+const { test } = require('@japa/runner')
+const { Macroable } = require('../dist')
+
+class Parent extends Macroable {
+  firstname () {
+    return 'super'
+  }
+}
+
+class Child extends Macroable {}
+
+test.group('Macroable', (group) => {
+  test('macro', () => {
+    function macroFn () {}
+    Parent.macro('macro', macroFn)
+
+    expect(Parent.getMacro('macro')).toEqual(macroFn)
+    expect((new Parent()).macro).toEqual(macroFn)
+  })
+
+  test('macro does not add a static function', () => {
+    Parent.macro('staticFn', () => {})
+
+    expect(Parent.staticFn).toBeUndefined()
+    expect((new Parent()).staticFn).toBeDefined()
+  })
+
+  test('macro ensures the class binding', () => {
+    Parent.macro('fullname', function () {
+      return this.firstname() + 'charge'
+    })
+
+    const fullname = new Parent().fullname()
+    expect(fullname).toEqual('supercharge')
+  })
+
+  test('throws when creating a macro without name', () => {
+    expect(() => {
+      Parent.macro()
+    }).toThrow('The first argument of ".macro(name, callback)" must be a valid string')
+  })
+
+  test('throws when creating a macro with an empty name', () => {
+    expect(() => {
+      Parent.macro('')
+    }).toThrow('The first argument of ".macro(name, callback)" must be a valid string')
+  })
+
+  test('throws when creating a macro without a function', () => {
+    expect(() => {
+      Parent.macro('name')
+    }).toThrow('The second argument ".macro(name, callback)" must be a function')
+  })
+
+  test('throws when creating a macro without function', () => {
+    expect(Parent.hasMacro('notExisting')).toBe(false)
+  })
+
+  test('hasMacro', () => {
+    Parent.macro('macroFn', () => {})
+    expect(Parent.hasMacro('macroFn')).toBe(true)
+    expect(Parent.hasMacro('notExisting')).toBe(false)
+  })
+
+  test('flushMacros', () => {
+    Parent.macro('macroFn', () => {})
+
+    expect(Parent.hasMacro('macroFn')).toBe(true)
+
+    Parent.flushMacros()
+    expect(Parent.hasMacro('macroFn')).toBe(false)
+  })
+
+  test('classes do not share macros', () => {
+    Child.macro('childMacro', () => {})
+    Parent.macro('parentMacro', () => {})
+
+    expect(Child.hasMacro('childMacro')).toBe(true)
+    expect(Child.hasMacro('parentMacro')).toBe(false)
+
+    expect(Parent.hasMacro('childMacro')).toBe(false)
+    expect(Parent.hasMacro('parentMacro')).toBe(true)
+  })
+})
